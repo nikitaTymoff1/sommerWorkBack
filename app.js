5f3c95d0c1a5fe153243551e1063aa3f56656c5c
nikitaTymoff1 <nikita.tymoff@gmail.com>
2020-05-27 15:59:11 +0300

    cors error 4

--- app.js
+++ app.js
@@ -22,11 +22,7 @@ require('./config/passport')(passport);
 app.use(passport.initialize());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(function(request, response, next) {
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./routes'));
 
