83ac3e515cb12074b8502fa45b91b57caa17f91c
nikitaTymoff1 <nikita.tymoff@gmail.com>
2020-05-27 20:52:17 +0300

    database fix

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://nikita:nikita@cluster0-nzp25.gcp.mongodb.net/courseBase', {useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://nikita:nikita@cluster0-nzp25.gcp.mongodb.net/courseBase', {useNewUrlParser: true,useUnifiedTopology: true});
 mongoose.connection.on('connected', () => {
     console.log('Database connected');
 });
