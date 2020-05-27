16906894c5099a5c93d2570b8650a8dfd5f6b4c9
nikitaTymoff1 <nikita.tymoff@gmail.com>
2020-05-27 15:41:38 +0300

    cors error

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ app.use(passport.initialize());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./routes'));
 
